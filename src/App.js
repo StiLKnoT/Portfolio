137a6a2efbec9cda59625789e9cf72562acafd54
StiLKnoT <stilknot@gmail.com>
2022-04-26 12:04:47 +0500

    nur

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import About from './About/About';
+import Service from './Service/Service';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <About/>
+      <Service/>
 
     </div>
   );
