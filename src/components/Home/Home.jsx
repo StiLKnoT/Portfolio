8f10b1293a281b35370b03ca95705a880c9f605f
StiLKnoT <stilknot@gmail.com>
2022-06-02 20:42:05 +0500

    added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import { motion } from 'framer-motion'
-import logo from "../../img/logo.png"
+import logo from "../../img/snake.png"
 import About from '../About/About'
 
 
@@ -18,24 +18,34 @@ const textAnimation = {
 }
 
 function Home() {
+    // window.addEventListener("scroll", function(){
+    //     const header = this.document.querySelector(".header");
+    //     header.classList.toggle("active", this.window.scrollY>0)
+    // })
   return (
     <div className="home">
         <div className="home__bg">
-            <div className="header d__flex align__items__center pxy__30">
-                <div className="logo">
-                    <img src={logo} alt="" />
-                </div>
-                <div className="navigation pxy__30">
-                    <ul className="navbar d__flex">
-                        <a href="#Home"><li className="nav__items mx__15">Home</li></a>
-                        <a href="#About"><li className="nav__items mx__15">About</li></a>
-                        <a href="#Services"><li className="nav__items mx__15">Services</li></a>
-                        <a href="#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
-                        <a href="#Blog"><li className="nav__items mx__15">Blog</li></a>
-                        <a href="#Contact"><li className="nav__items mx__15">Contact</li></a>
-
-                    </ul>
-
+            <div className="container header d__flex align__items__center pxy__30">
+                <div className="row">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div className="navigation pxy__30">
+                        <ul className="navbar d__flex">
+                            <a href="#Home"><li className="col-12 nav__items mx__15">Home</li></a>
+                            <a href="#About"><li className="col-12 nav__items mx__15">About</li></a>
+                            <a href="#Services"><li className="col-12 nav__items mx__15">Services</li></a>
+                            <a href="#Portfolio"><li className="col-12 nav__items mx__15">Portfolio</li></a>
+                            <a href="#Blog"><li className="col-12 nav__items mx__15">Blog</li></a>
+                            <a href="#Contact"><li className="col-12 nav__items mx__15">Contact</li></a>
+                        </ul>
+                    </div>
+                    {/* Toggle menu  */}
+                    {/* <div className="toggle_menu">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify white pointer" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                    </div> */}
                 </div>
             </div>
             {/* HOME CONTENT */}
